9bf0fffd0d18efc1422bbfabc247bc0d3a32d28c
Dragos Martac <dragosmartac@meta.com>
2024-12-20 16:54:58 +0200

    Added test file

--- src.py
+++ src.py
@@ -82,4 +82,5 @@ def main() -> None:
     print("-" * 50)
 
 
-main()
+if __name__ == "__main__":
+    main()
---
+++ test_src.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import unittest
+
+from datetime import datetime
+
+from src import parse_date
+
+
+class TestScript(unittest.TestCase):
+    def test_parse_date(self):
+        self.assertEqual(parse_date("1 Jan 2023"), datetime(2023, 1, 1))
+        self.assertEqual(parse_date("01 Jan 2023"), datetime(2023, 1, 1))
+        self.assertEqual(parse_date("31 Dec 2023"), datetime(2023, 12, 31))
+
+
+if __name__ == "__main__":
+    unittest.main()
